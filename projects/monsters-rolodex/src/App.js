14c930817d35b6a3026ef7ba8bf324aff0396c93
gururajkl <gururajragavendra7@gmail.com>
2024-12-31 13:58:07 +0530

    Refactor App component to use functional components and hooks, simplify CardList and SearchBox components

--- projects/monsters-rolodex/src/App.js
+++ projects/monsters-rolodex/src/App.js
@@ -1,57 +1,46 @@
-import { Component } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import CardList from "./components/card-list/card-list.component";
 import SearchBox from "./components/search-box/search-box.component";
 
-class App extends Component {
-  // 1st this method will run.
-  constructor() {
-    super();
+const App = () => {
+  const [searchfield, setSearchfield] = useState("");
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonster, setFilteredMonster] = useState(monsters);
 
-    this.state = {
-      monsters: [],
-      searchFiled: "",
-    };
-  }
+  console.log("render");
 
-  // Life cycle method in react when compenent is mounted (loads).
-  // 3rd this method will run.
-  componentDidMount() {
+  useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((users) =>
-        this.setState(() => {
-          return { monsters: users };
-        })
-      );
-  }
+      .then((users) => setMonsters(users));
+  }, []); // if there is no dependency array (2nd argument), this will run only once when the component is mounted.
 
-  onSearchChange = (event) => {
-    const searchFiled = event.target.value.toLowerCase();
-    this.setState({ searchFiled: searchFiled });
+  useEffect(() => {
+    const newFilteredMonster = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchfield);
+    });
+    setFilteredMonster(newFilteredMonster);
+  }, [monsters, searchfield]);
+
+  const onSearchChange = (event) => {
+    const searchFiledString = event.target.value.toLowerCase();
+    setSearchfield(searchFiledString);
   };
 
-  // 2nd this method will run.
-  render() {
-    const { monsters, searchFiled } = this.state;
-    const { onSearchChange } = this;
+  return (
+    <div className="App">
+      <h1 className="app-title">Monsters Rolodex</h1>
 
-    const filteredMonster = monsters.filter((monster) => {
-      return monster.name.toLowerCase().includes(searchFiled);
-    });
+      <SearchBox
+        className="monster-search-box"
+        onChangeHandler={onSearchChange}
+        placeholder="search monsters"
+      />
 
-    return (
-      <div className="App">
-        <h1 className="app-title">Monsters Rolodex</h1>
-        <SearchBox
-          className="monster-search-box"
-          onChangeHandler={onSearchChange}
-          placeholder="search monsters"
-        />
-        <CardList monsters={filteredMonster} />
-      </div>
-    );
-  }
-}
+      <CardList monsters={filteredMonster} />
+    </div>
+  );
+};
 
 export default App;
