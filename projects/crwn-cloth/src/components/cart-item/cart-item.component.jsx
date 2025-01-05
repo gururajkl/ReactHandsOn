b08f052f4e3d77e66e0c89ce3cc0d7ff74070df7
gururajkl <gururajragavendra7@gmail.com>
2025-01-05 20:53:35 +0530

    Enhance cart functionality: display item count in CartIcon and update CartItem component to show image and price details

--- projects/crwn-cloth/src/components/cart-item/cart-item.component.jsx
+++ projects/crwn-cloth/src/components/cart-item/cart-item.component.jsx
@@ -1,11 +1,16 @@
 import "./cart-item.styles.scss";
 
 const CartItem = ({ cartItem }) => {
-  const { name, quantity } = cartItem;
+  const { name, imageUrl, quantity, price } = cartItem;
   return (
-    <div>
-      <h2>{name}</h2>
-      <span>{quantity}</span>
+    <div className="cart-item-container">
+      <img alt={`${name}`} src={imageUrl} />
+      <div className="item-details">
+        <span className="name">{name}</span>
+        <span className="price">
+          {quantity} x ₹{price}
+        </span>
+      </div>
     </div>
   );
 };
